30f215b5ac42505aaf8b08c697d4689e62d2aeb2
Davey Struijk <mail@daveystruijk.com>
2019-02-09 16:48:02 +0100

    Refactor Game class

--- MinesweeperV1.py
+++ MinesweeperV1.py
@@ -9,108 +9,104 @@ import sys
 
 Move = collections.namedtuple('Person', 'action x y')
 
-# TODO: Make Cell
+
+class MineHitException(Exception):
+    pass
 
 
 class Game:
     def __init__(self):
-        #The cell value is (0 for nothing, 1-8 for #neighboring mines count)
-        self.gridplayed= Grid()
-        self.game_end = False
+        self.grid = Grid()
         self.score = 0
         self.round_number = 0
 
-    def set_grid_played(self):
-        self.gridplayed.calculate_number_neigboring_mines()
-
-    def ask_user_input_coordinates_and_action(self):
-        # user sends x,y coordinates + action reveal or action flag
-        pass
-
     def run_game(self):
-        self.gridplayed.add_mines()
-        self.set_grid_played()
-        self.gridplayed.show_grid()
-        while not self.game_end:
+        self.grid.generate_mines()
+        self.grid.update_number_neigboring_mines()
+        self.grid.show_grid()
+        while True:
             self.run_round()
 
     def run_round(self):
         self.round_number += 1
         next_move = self.user_input()
-        self.gridplayed.reveal_cell(next_move.x, next_move.y)
-        self.check_end_of_game()
+        try:
+            self.grid.reveal_cell(next_move.x, next_move.y)
+            self.grid.show_grid()
+        except MineHitException:
+            self.grid.show_grid()
+            self.end_of_game(won=False)
+
+        # Update score
+        revealed_cells_count = self.grid.calculate_number_cells_revealed()
+        self.score = revealed_cells_count
+
+        if revealed_cells_count > self.grid.total_revealablecells:
+            self.end_of_game(won=True)
+
         print("Round number:", self.round_number)
-        self.gridplayed.show_grid()
 
     def user_input(self):
         x = int(input("Enter x value"))
         y = int(input("Enter y value"))
         return Move(action='flag', x=x, y=y)
 
-    def check_end_of_game(self):
-        # check if mine is revealed if so game ends
-        if(self.gridplayed.check_if_mine_is_revealed()):
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            print("Game has ended because you revealed a mine, your score is:")
-            print(self.score)
-            sys.exit()
+    def end_of_game(self, won=False):
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        if won:
+            print("You win!!!! , your score is:")
         else:
-            # check if all revealable cells are revealed
-            self.gridplayed.calculate_number_cells_revealed()
-            self.score = self.gridplayed.numberofrevealedcells
-            if(self.gridplayed.numberofrevealedcells == self.gridplayed.total_revealablecells):
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-                print("You win!!!! , your score is:")
-                print(self.score)
-                sys.exit()
+            print("Game has ended because you revealed a mine, your score is:")
+        print(self.score)
+        sys.exit()
 
 
 class Grid:
     def __init__(self):
-        self.gridwidth = 10
-        self.gridheight = 10
+        self.width = 10
+        self.height = 10
         self.numberofmines = 10
         self.numberofrevealedcells = 0
-        self.numberofcells = self.gridwidth * self.gridheight
+        self.numberofcells = self.width * self.height
         self.total_revealablecells = self.numberofcells - self.numberofmines
-        # Makes a list containing number of <gridheight> lists, each of number
-        # of <gridwidth> items. All items == cell instance
-        self.matrix = [[Cell() for x in range(self.gridwidth)]
-                       for y in range(self.gridheight)]
+        # Makes a list containing number of <height> lists, each of number
+        # of <width> items. All items == cell instance
+        self.matrix = [[Cell() for x in range(self.width)]
+                       for y in range(self.height)]
 
     def show_grid(self):
         print("  ", end="")
-        for i in range(0, self.gridheight):  # add numbers to coloms
+        for i in range(0, self.height):  # add numbers to coloms
             print("|", end="")
             print(i, end="")
         print("|", end="")
-        for y in range(0, self.gridheight):  # add numbers to the rows
+        for y in range(0, self.height):  # add numbers to the rows
             print("")
             print(y, end="")
             print("|", end="")
-            for x in range(0, self.gridwidth):
+            for x in range(0, self.width):
                 print("|", end="")
                 self.matrix[x][y].output_cell_value()
             print("|", end="")
         print("")
 
-    def add_mines(self):
+    def generate_mines(self):
         numberofminesadded = 0
         while (numberofminesadded < self.numberofmines):
-            y = random.randint(0, self.gridheight - 1)
-            x = random.randint(0, self.gridwidth - 1)
+            y = random.randint(0, self.height - 1)
+            x = random.randint(0, self.width - 1)
             if not self.matrix[x][y].mine:
-                self.matrix[x][y].add_mine()
+                self.matrix[x][y].mine = True
                 numberofminesadded += 1
 
-    def calculate_number_neigboring_mines(self):
-        for y in range(self.gridheight):
-            for x in range(self.gridwidth):
-                if (self.matrix[x][y].check_if_cell_contains('mine')):
+    def update_number_neigboring_mines(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                if (self.matrix[x][y].mine):
                     pass
                 else:
                     self.matrix[x][y].add_number_neighboring_mines(
-                        self.calculate_number_in_neighborhood(x, y, 'mine'))
+                        self.calculate_number_of_mines_in_neighborhood(x, y))
 
     def reveal_cell(self, x, y):
         cell = self.matrix[x][y]
@@ -118,19 +114,22 @@ class Grid:
             return
         cell.revealed = True
 
+        if cell.mine:
+            raise MineHitException
+
         # If not empty, stop searching
-        if not cell.check_if_cell_contains('0'):
+        if not cell.neighboring_mines_count == 0:
             return
 
         for yi in range(y - 1, y + 2):
             for xi in range(x - 1, x + 2):
                 # Don't search outside of grid
-                if(xi < 0 or yi < 0 or xi >= self.gridwidth or yi >= self.gridheight):
+                if(xi < 0 or yi < 0 or xi >= self.width or yi >= self.height):
                     continue
                 # Recurse!
                 self.reveal_cell(xi, yi)
 
-    def calculate_number_in_neighborhood(self, x, y, checkvalue):
+    def calculate_number_of_mines_in_neighborhood(self, x, y):
         numberofneighbors = 0
 
         # take it into consideration that the start of the array is at 0, so
@@ -140,28 +139,29 @@ class Grid:
 
                 # if outside of range (@borders of grid) don't check just
                 # continue
-                if(xi < 0 or yi < 0 or xi >= self.gridwidth or yi >= self.gridheight):
+                if(xi < 0 or yi < 0 or xi >= self.width or yi >= self.height):
                     continue
                 else:  # if within range then check if neighbors cells contain checkvalue and increment the number of total neighbors
-                    if (self.matrix[xi][yi].check_if_cell_contains(checkvalue)):
+                    if (self.matrix[xi][yi].mine):
                         numberofneighbors += 1
         return numberofneighbors
 
     def check_if_mine_is_revealed(self):
-        for y in range(self.gridheight):
-            for x in range(self.gridwidth):
-                if(self.matrix[x][y].check_if_cell_contains('mine') and self.matrix[x][y].revealed):
+        for y in range(self.height):
+            for x in range(self.width):
+                if(self.matrix[x][y].mine and self.matrix[x][y].revealed):
                     return True
         return False
 
     def calculate_number_cells_revealed(self):
         # is het beter om lokaal een nieuwe variabele aan te maken of gewoon
         # deze even op 0 te zetten?
-        self.numberofrevealedcells = 0
-        for y in range(self.gridheight):
-            for x in range(self.gridwidth):
+        numberofrevealedcells = 0
+        for y in range(self.height):
+            for x in range(self.width):
                 if(self.matrix[x][y].revealed):
-                    self.numberofrevealedcells += 1
+                    numberofrevealedcells += 1
+        return numberofrevealedcells
 
 
 class Cell:
@@ -170,41 +170,18 @@ class Cell:
         self.revealed = False
         self.neighboring_mines_count = 0
 
-    def add_mine(self):
-        self.mine = True
+    # hier is een voorbeeld van een functie die andere functies in zelfde
+    # level aanroept (nested structuur). Is dat good practice of juist
+    # niet?
+    def output_cell_value(self):
+        if not self.revealed:
+            print("*", end="")
+            return
 
-    def reveal_cell(self):
-        self.revealed = True
-
-    def hide_cell(self):
-        print("*", end="")
-
-    def show_cell(self):
         if self.mine:
             print("x", end="")
         else:
             print(self.neighboring_mines_count, end="")
-
-        # hier is een voorbeeld van een functie die andere functies in zelfde
-        # level aanroept (nested structuur). Is dat good practice of juist
-        # niet?
-    def output_cell_value(self):
-        if self.revealed:
-            self.show_cell()
-        else:
-            self.hide_cell()
-
-    def check_if_cell_contains(self, checkvalue):
-        if (checkvalue == 'mine'):
-            if self.mine:
-                return True
-            else:
-                return False
-        if (checkvalue == '0'):
-            if self.neighboring_mines_count == 0 and not self.mine:
-                return True
-            else:
-                return False
 
         # is dit de nette manier of juist de omslachtige manier om
         # numberofneigbhborsmines te updaten?
